a0f42b72450c219ad5ee2b8663fadae33536d734
R2 D2 <p234a137@gmail.com>
2015-04-24 14:01:19 -0700

    small updates to lists and final finished. produce a histogram of registration vs time

--- build_from_mailinglist.R
+++ build_from_mailinglist.R
@@ -10,22 +10,32 @@ cat(rep('#',80), sep="")
 cat("\n")
 cat("FullName", ", ", "Email", ", ",  "Institute", ", ",  "Collaboration", fill = T)
 cat("\n")
+dates <- vector()
 for(i in 1:nrow(mailinglist_usluo)){
 #for(i in 1:20){
   row = mailinglist_usluo[i,]
   #print(registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 1:5], row.names=F, col.names = F, collab="")
   fullName  <- registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 1 ]
-  email     <- tolower(registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 2 ])
-  institute <- registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 3 ]
-  collab    <- registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 4 ]
-  if ( length(collab)==0 || ! collab %in% list_of_collaborations){
-    collab = "---"
-  }
   if(length(fullName) > 0){
-  cat(fullName[1], ", ", email[1], ", ", institute[1], ", ", collab[1], "\n", fill = F)
+    email     <- tolower(registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 2 ])
+    institute <- registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 3 ]
+    collab    <- registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 4 ]
+    datetime  <- strsplit(registration_list[ tolower(registration_list$Email) == tolower(row$Email) , 6 ], " ")[[1]][1]
+    if ( length(collab)==0 || ! collab %in% list_of_collaborations){
+      collab = "---"
+    }
+    dates <- append(dates, as.Date(datetime[1]))
+    cat(fullName[1], ", ", email[1], ", ", institute[1], ", ", collab[1], ",", datetime[1], "\n", fill = F)
   }
 #   if(length(fullName)==0){
 #     print(row$Email)
 #     print(row$fullName)
 #   }
 }
+
+# create png file
+png(filename = "uslua.png", width = 480, height = 960, units = "px")
+par(mfcol = c(2, 1))
+hist(dates, breaks="quarters", freq=TRUE, main = "USLUA Registrations", xlab = "Time (bins = years)")
+hist(dates, breaks="months", freq=TRUE, main = "USLUA Registrations", xlab = "Time (bins = months)")
+dev.off()
